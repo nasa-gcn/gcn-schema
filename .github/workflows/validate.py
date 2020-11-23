c94d7aabb37af3ab4e7bb320a2459baceb1b6b6c
Leo Singer <leo.singer@ligo.org>
2020-11-23 10:57:03 -0500

    Fail CI pipeline if parsing fails

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 from json.decoder import JSONDecodeError
 from glob import iglob
+import sys
+
 from fastavro.schema import load_schema, SchemaParseException, UnknownType
+
+failed = False
 
 for filename in iglob('*.avsc'):
     try:
         load_schema(filename)
     except (JSONDecodeError, SchemaParseException) as e:
         print(f'error: {filename}: {e.args[0]}')
+        failed = True
     except UnknownType as e:
         print(f'error: {filename}: unknown type: {e.args[0]}')
+        failed = True
+
+if failed:
+    sys.exit(1)
