65f3e97858a5aa04d3a907a7b1caa3679dac9237
Leo Singer <leo.singer@ligo.org>
2021-09-22 15:23:06 -0400

    Revamp CI script to output GNU-style error messages

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+Parse all Avro schema in this repository.
+
+Errors are output in the GNU error format
+<https://www.gnu.org/prep/standards/html_node/Errors.html>.
+"""
 from json.decoder import JSONDecodeError
 from glob import iglob
 import sys
@@ -10,11 +16,13 @@ failed = False
 for filename in iglob('*.avsc'):
     try:
         load_schema(filename)
-    except (JSONDecodeError, SchemaParseException) as e:
-        print(f'error: {filename}: {e.args[0]}')
+    except JSONDecodeError as e:
+        print(f'{filename}:{e.lineno}:{e.colno}: error: {e.msg}')
+    except SchemaParseException as e:
+        print(f'{filename}: error: {e.args[0]}')
         failed = True
     except UnknownType as e:
-        print(f'error: {filename}: unknown type: {e.args[0]}')
+        print(f'{filename}: error: unknown type: {e.args[0]}')
         failed = True
 
 if failed:
