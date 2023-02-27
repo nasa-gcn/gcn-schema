aacf3c0f1bfe9fc0dd4d632838dcf426d796c7a4
dakota002 <dakota.d@live.com>
2023-02-27 16:24:42 -0500

    Just validation stuff

--- validate.mjs
+++ validate.mjs
@@ -1,25 +1,28 @@
 import { readFile } from 'fs/promises'
 import Ajv from 'ajv/dist/2019.js'
 import meta from 'ajv/dist/refs/json-schema-2020-12/index.js'
+import { glob } from 'glob'
 
 const ajv = new Ajv({ verbose: true })
 ajv.addMetaSchema(meta, 'https://json-schema.org/draft/2020-12/schema')
 
 /** @param {string} path */
 async function validate(path) {
-  const text = await readFile(path, { encoding: 'utf-8' })
-  const schema = JSON.parse(text)
-  try {
-    ajv.compile(schema)
-    return true
-  } catch (e) {
-    if (e instanceof Error) {
-      process.exitCode = 1
-      console.error(`${path}: ${e.message}`)
-    } else {
-      throw e
-    }
-  }
+  const filesInDir = await glob(path, {
+    ignore: ['test/**', 'node_modules/**'],
+  })
+
+  const schemas = await Promise.all(
+    filesInDir.map(async (file) =>
+      JSON.parse(
+        await readFile(file, {
+          encoding: 'utf-8',
+        })
+      )
+    )
+  )
+  ajv.addSchema(schemas).compile(true)
+  return true
 }
 
 const args = process.argv.slice(2)
